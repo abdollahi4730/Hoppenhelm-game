7f1d75258fd8c43d014434030cefa929586a5d8b
Mohammad <mohammadmohammad0854@gmail.com>
2024-05-22 00:57:43 +0330

    change background color to lightGreen

--- src/main/java/com/example/hoppenhelm/HelloApplication.java
+++ src/main/java/com/example/hoppenhelm/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.hoppenhelm;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 920, 720);
+        scene.setFill(Color.LIGHTGREEN);
+//        layout
+        stage.setTitle("Hello! title");
         stage.setScene(scene);
         stage.show();
     }
