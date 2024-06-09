71c6a5497c43e78c8c164f9100afe0ba1a3e4ec6
zahraew <zahrashafaati33@gmail.com>
2024-06-09 18:23:01 +0330

    Playground update

---
+++ Hoppenhelm-game-main (1)/Hoppenhelm-game-main/README.md
@@ -0,0 +1,8 @@
+hello this is a test
+this test from emad
+
+
+yoyo gang mohandes shafaati
+
+yoyo gang mohandes 2 yadollahpour
+.
---
+++ Hoppenhelm-game-main (1)/Hoppenhelm-game-main/src/main/java/com/game/hoppenhelm/Player.java
@@ -0,0 +1,65 @@
+package com.game.hoppenhelm;
+import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
+import javafx.animation.AnimationTimer;
+import javafx.scene.shape.Rectangle; // for use in earth of game
+import java.lang.Thread;
+
+public class Player {
+    // constructor
+    private int CenterX , CenterY , Radius ;
+    private AnimationTimer timer;
+    private double targetY , currentY , deltaY;
+    Circle circle;
+    public Player(int CenterX , int CenterY , int Radius) {
+        this.CenterX = CenterX ;
+        this.CenterY = CenterY ;
+        this.Radius = Radius ;
+
+//        System.out.println(this.CenterX + " " + this.CenterY + " " + this.Radius);
+        this.circle = new Circle();
+        this.circle.setCenterX(80);
+        this.circle.setFill(Color.LIGHTGREEN);
+        this.circle.setCenterY(650);
+        this.circle.setRadius(50);
+
+    }
+    public Circle getCircle() {
+        return circle;
+    }
+    public void moveCircle() throws InterruptedException {
+        System.out.println("hello");
+        double nowLocationY = circle.getCenterY();
+
+        circle.setCenterY(nowLocationY -100 );
+//        circle.getTransl
+//        Thread.sleep(1000);
+
+//        for(double i = nowLocationY ; i > nowLocationY - 200 ; i = i - 8) {
+//            circle.setCenterY(i);
+//            Thread.sleep(1);
+//        }
+//        Thread.sleep(500);
+//        currentY = circle.getCenterY();
+//        for(double i = currentY ; i < currentY + 200 ; i = i + 8) {
+//            circle.setCenterY(i);
+//            Thread.sleep(1 , 1);
+////            Thread.sleep();
+//        }
+//        System.out.println("bye");
+
+//        currentY = circle.getCenterY();
+//        targetY = currentY - 100;
+//        deltaY = targetY - currentY;
+//
+//        this.timer = new AnimationTimer() {
+//            @Override
+//            public void handle(long now) {
+//
+//                double speed = 5;
+//                circle.setCenterY(targetY);
+////                timer.stop();
+//            }
+//        };
+    }
+}
