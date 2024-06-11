28bd0bed789b7db6243216323162cee564638d3a
zahraew <zahrashafaati33@gmail.com>
2024-06-11 15:48:13 +0330

    remove

--- Hoppenhelm-game-main (1)/Hoppenhelm-game-main/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.hoppenhelm {
-    requires javafx.controls;
-    requires javafx.fxml;
-//    requires java.desktop;
-
-
-    opens com.game.hoppenhelm to javafx.fxml;
-    exports com.game.hoppenhelm;
-}
