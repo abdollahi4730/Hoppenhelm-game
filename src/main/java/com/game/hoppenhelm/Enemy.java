8fae43b65a57e69077476f265fcd268c6192817f
Mohammad <mohammadmohammad0854@gmail.com>
2024-06-24 13:41:42 +0330

    update style enemy and update moverectangle function

--- src/main/java/com/game/hoppenhelm/Enemy.java
+++ src/main/java/com/game/hoppenhelm/Enemy.java
@@ -2,7 +2,7 @@ package com.game.hoppenhelm;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-
+//import javafx.scene.shape.
 import javafx.scene.shape.Circle;
 
 public class Enemy {
@@ -13,10 +13,13 @@ public class Enemy {
         this.heightScreen = heightScreen;
         this.widthScreen = widthScreen;
 
+
         this.rectangle = new Rectangle();
         this.rectangle.setHeight(heightScreen);
         this.rectangle.setWidth(widthScreen);
-        this.rectangle.setFill(Color.RED);
+//        this.rectangle.setFill(Color.GREEN);
+        this.rectangle.setStyle("-fx-fill: green;-fx-arc-height: 50; -fx-arc-width:50;-fx-stroke: white;-fx-stroke-width: 2; ");
+//        this.rectangle.setStyle("-fx-arc-height: 70; -fx-arc-width:70;-fx-stroke: white;-fx-stroke-width: 3; ");
         this.rectangle.setY(CenterY);
         this.rectangle.setX(CenterX);
         this.CenterX = CenterX;
@@ -33,7 +36,7 @@ public class Enemy {
     public void moverectangle() throws InterruptedException {
         System.out.println("hello");
 
-        CenterX -=178;
+        CenterX -=148;
         rectangle.setX(CenterX);
         System.out.println(CenterX);
     }
@@ -55,7 +58,9 @@ public class Enemy {
         this.rectangle = new Rectangle();
         this.rectangle.setHeight(heightScreen);
         this.rectangle.setWidth(widthScreen);
-        this.rectangle.setFill(Color.RED);
+//        this.rectangle.setFill(Color.RED);
+//        this.rectangle.setFill(Color.GREEN);
+        this.rectangle.setStyle("-fx-fill: green;-fx-arc-height: 50; -fx-arc-width:50;-fx-stroke: white;-fx-stroke-width: 2; ");
         this.rectangle.setY(CenterY);
         this.rectangle.setX(CenterX);
         this.CenterX = CenterX;
